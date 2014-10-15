9f26795698f135a91ed1f99afe3f2389be899ff0
zxh0 <zhangxiuhong2013@gmail.com>
2014-10-15 16:41:10 +0800

    add server.Main

---
+++ akka-examples/mini-rpg-json/src/main/java/com/github/zxh/akka/minirpg/server/Main.java
@@ -0,0 +1,45 @@
+package com.github.zxh.akka.minirpg.server;
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.io.Tcp;
+import akka.util.ByteIterator;
+import akka.util.ByteString;
+import akka.util.ByteStringBuilder;
+import java.nio.ByteOrder;
+import java.util.Arrays;
+
+public class Main {
+    
+    public static void main(String[] args) {
+//        ActorSystem mySystem = ActorSystem.create("mySystem");
+//        ActorRef tcpManager = Tcp.get(mySystem).getManager();
+//        Props accepterProps = null;//Props.create(Accepter.class, tcpManager);
+//        ActorRef accepter = mySystem.actorOf(accepterProps, "accepter");
+//        accepter.tell(12345, ActorRef.noSender());
+        
+//        ByteStringBuilder bsb = new ByteStringBuilder();
+//        bsb.putInt(2, ByteOrder.BIG_ENDIAN);
+//        bsb.putInt(3, ByteOrder.BIG_ENDIAN);
+//        bsb.putInt(4, ByteOrder.BIG_ENDIAN);
+//        
+//        ByteString bs = bsb.result();
+//        System.out.println(bs.take(4));
+//        bs.drop(4);
+//        System.out.println(bs.take(4));
+//        System.out.println(bs.length());
+//        
+//        ByteIterator bi = bs.iterator();
+//        System.out.println(bi.getInt(ByteOrder.BIG_ENDIAN));
+//        System.out.println(bi.getInt(ByteOrder.BIG_ENDIAN));
+//        System.out.println(bi.getInt(ByteOrder.BIG_ENDIAN));
+//        
+//        System.out.println(bs.take(4));
+//        System.out.println(bs.length());
+        
+        ByteString bs = ByteString.fromString("abcdefg");
+        System.out.println(new String(bs.slice(1, 3).toArray()));
+    }
+    
+}
--- akka-examples/mini-rpg-json/src/main/java/com/github/zxh/akka/minirpg/server/MsgHandler.java
+++ akka-examples/mini-rpg-json/src/main/java/com/github/zxh/akka/minirpg/server/MsgHandler.java
@@ -1,14 +1,36 @@
 package com.github.zxh.akka.minirpg.server;
 
 import akka.actor.UntypedActor;
+import com.github.zxh.akka.minirpg.domain.Player;
+import com.github.zxh.akka.minirpg.message.CreatePlayerRequest;
+import com.github.zxh.akka.minirpg.message.CreatePlayerResponse;
+import java.util.ArrayList;
+import java.util.List;
 
+// game logic
 public class MsgHandler extends UntypedActor {
+    
+    private final List<Player> players = new ArrayList<>();
 
     @Override
     public void onReceive(Object msg) throws Exception {
         System.out.println("MsgHandler received:" + msg);
         
         // todo
+        if (msg instanceof CreatePlayerRequest) {
+            int newPlayerId = createPlayer((CreatePlayerRequest) msg);
+            getSender().tell(new CreatePlayerResponse(newPlayerId), getSelf());
+        }
+    }
+    
+    private int createPlayer(CreatePlayerRequest req) {
+        int playerId = players.size() + 1;
+        Player newPlayer = new Player();
+        newPlayer.setId(playerId);
+        newPlayer.setLevel(1);
+        newPlayer.setName(req.getPlayerName());
+        players.add(newPlayer);
+        return playerId;
     }
     
 }
