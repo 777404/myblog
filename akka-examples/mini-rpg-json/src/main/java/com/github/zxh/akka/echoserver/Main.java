ffa9887d6d465e9f95f69d548e57141cfd9c2077
zxh0 <zhangxiuhong2013@gmail.com>
2014-10-15 16:22:52 +0800

    finish MsgCodec

--- akka-examples/mini-rpg-json/src/main/java/com/github/zxh/akka/echoserver/Main.java
+++ akka-examples/mini-rpg-json/src/main/java/com/github/zxh/akka/echoserver/Main.java
@@ -19,24 +19,27 @@ public class Main {
 //        ActorRef accepter = mySystem.actorOf(accepterProps, "accepter");
 //        accepter.tell(12345, ActorRef.noSender());
         
-        ByteStringBuilder bsb = new ByteStringBuilder();
-        bsb.putInt(2, ByteOrder.BIG_ENDIAN);
-        bsb.putInt(3, ByteOrder.BIG_ENDIAN);
-        bsb.putInt(4, ByteOrder.BIG_ENDIAN);
+//        ByteStringBuilder bsb = new ByteStringBuilder();
+//        bsb.putInt(2, ByteOrder.BIG_ENDIAN);
+//        bsb.putInt(3, ByteOrder.BIG_ENDIAN);
+//        bsb.putInt(4, ByteOrder.BIG_ENDIAN);
+//        
+//        ByteString bs = bsb.result();
+//        System.out.println(bs.take(4));
+//        bs.drop(4);
+//        System.out.println(bs.take(4));
+//        System.out.println(bs.length());
+//        
+//        ByteIterator bi = bs.iterator();
+//        System.out.println(bi.getInt(ByteOrder.BIG_ENDIAN));
+//        System.out.println(bi.getInt(ByteOrder.BIG_ENDIAN));
+//        System.out.println(bi.getInt(ByteOrder.BIG_ENDIAN));
+//        
+//        System.out.println(bs.take(4));
+//        System.out.println(bs.length());
         
-        ByteString bs = bsb.result();
-        System.out.println(bs.take(4));
-        bs.drop(4);
-        System.out.println(bs.take(4));
-        System.out.println(bs.length());
-        
-        ByteIterator bi = bs.iterator();
-        System.out.println(bi.getInt(ByteOrder.BIG_ENDIAN));
-        System.out.println(bi.getInt(ByteOrder.BIG_ENDIAN));
-        System.out.println(bi.getInt(ByteOrder.BIG_ENDIAN));
-        
-        System.out.println(bs.take(4));
-        System.out.println(bs.length());
+        ByteString bs = ByteString.fromString("abcdefg");
+        System.out.println(new String(bs.slice(1, 3).toArray()));
     }
     
 }
--- akka-examples/mini-rpg-json/src/main/java/com/github/zxh/akka/minirpg/server/MsgCodec.java
+++ akka-examples/mini-rpg-json/src/main/java/com/github/zxh/akka/minirpg/server/MsgCodec.java
@@ -10,6 +10,8 @@ import akka.util.ByteString;
 import akka.util.ByteStringBuilder;
 import com.github.zxh.akka.minirpg.message.GameMessage;
 import com.google.gson.Gson;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.nio.ByteOrder;
 import java.nio.charset.StandardCharsets;
 
@@ -18,10 +20,12 @@ public class MsgCodec extends UntypedActor {
     private static final Gson GSON = new Gson();
     
     private final ActorRef connection;
+    private final ActorRef msgHandler;
     private ByteString buf = ByteString.empty();
 
-    public MsgCodec(ActorRef connection) {
-        this.connection  = connection;
+    public MsgCodec(ActorRef connection, ActorRef msgHandler) {
+        this.connection = connection;
+        this.msgHandler = msgHandler;
     }
     
     @Override
@@ -36,20 +40,32 @@ public class MsgCodec extends UntypedActor {
         } else if (msg instanceof ConnectionClosed) {
             getContext().stop(getSelf());
         } else if (msg instanceof GameMessage) {
-            ByteString data = encodeMsg(msg);
+            final ByteString data = encodeMsg(msg);
             connection.tell(TcpMessage.write(data), getSelf());
         }
     }
     
     private void decodeMsg() {
-        while (buf.length() > 4) {
+        while (buf.length() > 8) {
             final ByteIterator it = buf.iterator();
-            final int msgLength = it.getInt(ByteOrder.BIG_ENDIAN);
-            if (buf.length() >= 4 + msgLength) {
-                final int msgId = it.getInt(ByteOrder.BIG_ENDIAN);
-                
+            final int msgId = it.getInt(ByteOrder.BIG_ENDIAN);
+            final int jsonLength = it.getInt(ByteOrder.BIG_ENDIAN);
+            
+            if (buf.length() >= 8 + jsonLength) {
+                final Object msg = decodeMsg(msgId, buf.slice(9, 9 + jsonLength));
+                buf = buf.drop(8 + jsonLength);
+                msgHandler.tell(msg, getSelf());
             }
         }
+    }
+    
+    private Object decodeMsg(int msgId, ByteString jsonData) {
+        final Class<?> msgClass = MsgRegistry.getMsgClass(msgId);
+        final Reader reader = new InputStreamReader(
+                jsonData.iterator().asInputStream(),
+                StandardCharsets.UTF_8);
+
+        return GSON.fromJson(reader, msgClass);
     }
     
     private ByteString encodeMsg(Object msg) {
@@ -58,8 +74,8 @@ public class MsgCodec extends UntypedActor {
                 .getBytes(StandardCharsets.UTF_8);
         
         final ByteStringBuilder bsb = new ByteStringBuilder();
-        bsb.putInt(4 + jsonBytes.length, ByteOrder.BIG_ENDIAN);
         bsb.putInt(msgId, ByteOrder.BIG_ENDIAN);
+        bsb.putInt(jsonBytes.length, ByteOrder.BIG_ENDIAN);
         bsb.putBytes(jsonBytes);
         
         return bsb.result();
