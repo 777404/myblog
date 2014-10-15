ecf3d23db8469f1e7fac1c395412471b1dcfb133
zxh0 <zhangxiuhong2013@gmail.com>
2014-10-15 14:32:43 +0800

    JsonMessage

--- akka-examples/mini-rpg-json/src/main/java/com/github/zxh/akka/echoserver/Main.java
+++ akka-examples/mini-rpg-json/src/main/java/com/github/zxh/akka/echoserver/Main.java
@@ -4,15 +4,30 @@ import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
 import akka.io.Tcp;
+import akka.util.ByteString;
+import akka.util.ByteStringBuilder;
+import java.util.Arrays;
 
 public class Main {
     
     public static void main(String[] args) {
-        ActorSystem mySystem = ActorSystem.create("mySystem");
-        ActorRef tcpManager = Tcp.get(mySystem).getManager();
-        Props accepterProps = null;//Props.create(Accepter.class, tcpManager);
-        ActorRef accepter = mySystem.actorOf(accepterProps, "accepter");
-        accepter.tell(12345, ActorRef.noSender());
+//        ActorSystem mySystem = ActorSystem.create("mySystem");
+//        ActorRef tcpManager = Tcp.get(mySystem).getManager();
+//        Props accepterProps = null;//Props.create(Accepter.class, tcpManager);
+//        ActorRef accepter = mySystem.actorOf(accepterProps, "accepter");
+//        accepter.tell(12345, ActorRef.noSender());
+        
+        ByteString bs = ByteString.empty();
+        ByteString bs2 = bs.concat(ByteString.fromString("abc"));
+        System.out.println(Arrays.toString(bs.toArray()));
+        System.out.println(Arrays.toString(bs2.toArray()));
+        
+//        bs2.tak
+        
+        ByteStringBuilder bsb = new ByteStringBuilder();
+        bsb.append(bs);
+        
+//        bsb.
     }
     
 }
--- akka-examples/mini-rpg-json/src/main/java/com/github/zxh/akka/minirpg/server/MsgCodec.java
+++ akka-examples/mini-rpg-json/src/main/java/com/github/zxh/akka/minirpg/server/MsgCodec.java
@@ -3,21 +3,35 @@ package com.github.zxh.akka.minirpg.server;
 import akka.actor.UntypedActor;
 import akka.io.Tcp.ConnectionClosed;
 import akka.io.Tcp.Received;
-import akka.io.TcpMessage;
 import akka.util.ByteString;
+import com.github.zxh.akka.minirpg.message.JsonMessage;
 
 public class MsgCodec extends UntypedActor {
 
+    private ByteString buf = ByteString.empty();
+    
     @Override
     public void onReceive(Object msg) throws Exception {
         System.out.println("MsgCodec received:" + msg);
         
         if (msg instanceof Received) {
             final ByteString data = ((Received) msg).data();
-            getSender().tell(TcpMessage.write(data), getSelf());
+            buf = buf.concat(data);
+            decodeMsg();
+            //getSender().tell(msg, getSelf());
         } else if (msg instanceof ConnectionClosed) {
             getContext().stop(getSelf());
+        } else if (msg instanceof JsonMessage) {
+            // todo
         }
+    }
+    
+    private void decodeMsg() {
+        // todo
+    }
+    
+    private void encodeMsg(JsonMessage msg) {
+        
     }
     
 }
