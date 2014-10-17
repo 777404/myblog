82c5f17e53e29e870bc577973ef480998cceaeb9
zxh0 <zhangxiuhong2013@gmail.com>
2014-10-17 12:01:59 +0800

    client.MsgHandler

--- akka-examples/mini-rpg-json/src/main/java/com/github/zxh/akka/minirpg/client/ClientApp.java
+++ akka-examples/mini-rpg-json/src/main/java/com/github/zxh/akka/minirpg/client/ClientApp.java
@@ -1,9 +1,23 @@
 package com.github.zxh.akka.minirpg.client;
 
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import java.net.InetSocketAddress;
+
 public class ClientApp {
     
     public static void main(String[] args) {
-        // todo
+        ActorSystem mySystem = ActorSystem.create("miniRPG");
+        ActorRef msgHandler = mySystem.actorOf(Props.create(MsgHandler.class));
+        ActorRef tcpClient = mySystem.actorOf(Props.create(TcpClient.class));
+        tcpClient.tell(new InetSocketAddress("localhost", 12345), ActorRef.noSender());
+        
+        String cmd;
+        while (true) {
+            cmd = System.console().readLine();
+            // todo
+        }
     }
     
 }
--- akka-examples/mini-rpg-json/src/main/java/com/github/zxh/akka/minirpg/client/MsgHandler.java
+++ akka-examples/mini-rpg-json/src/main/java/com/github/zxh/akka/minirpg/client/MsgHandler.java
@@ -1,25 +1,12 @@
 package com.github.zxh.akka.minirpg.client;
 
-import akka.actor.ActorRef;
-import akka.actor.ActorSystem;
-import akka.actor.Props;
 import akka.actor.UntypedActor;
-import java.net.InetSocketAddress;
 
 public class MsgHandler extends UntypedActor {
 
     @Override
     public void onReceive(Object msg) throws Exception {
-        ActorSystem mySystem = ActorSystem.create("miniRPG");
-        ActorRef msgHandler = mySystem.actorOf(Props.create(MsgHandler.class));
-        ActorRef tcpClient = mySystem.actorOf(Props.create(TcpClient.class));
-        tcpClient.tell(new InetSocketAddress("localhost", 12345), ActorRef.noSender());
-        
-        String cmd;
-        while (true) {
-            cmd = System.console().readLine();
-            // todo
-        }
+        // todo
     }
     
 }
