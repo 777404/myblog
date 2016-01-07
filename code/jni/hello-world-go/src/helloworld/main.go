49777898ff34963f2ecc730b2c6c471b1c43109b
zxh0 <zhangxiuhong2013@gmail.com>
2016-01-07 21:29:16 +0800

    main.go

--- code/jni/hello-world-go/src/helloworld/main.go
+++ code/jni/hello-world-go/src/helloworld/main.go
@@ -1,1 +1,15 @@
 package main
+
+// #cgo CFLAGS: -I/Library/Java/JavaVirtualMachines/jdk1.8.0_60.jdk/Contents/Home/include
+// #cgo CFLAGS: -I/Library/Java/JavaVirtualMachines/jdk1.8.0_60.jdk/Contents/Home/include/darwin
+// #include <jni.h>
+import "C"
+
+//export Java_HelloWorld_print
+func Java_HelloWorld_print(env *C.JNIEnv, obj C.jobject) {
+    println("Hello, World!")
+}
+
+func main() {}
+
+// go build -buildmode=c-shared -o libHelloWorld.dylib math.go
