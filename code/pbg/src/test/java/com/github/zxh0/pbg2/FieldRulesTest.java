60a581151e79c85a5c0bd9d5460c86df9aa67f8f
zxh0 <zhangxiuhong2013@gmail.com>
2016-01-09 16:07:55 +0800

    add FieldTypesTest.java

--- code/pbg/src/test/java/com/github/zxh0/pbg2/FieldRulesTest.java
+++ code/pbg/src/test/java/com/github/zxh0/pbg2/FieldRulesTest.java
@@ -1,6 +1,5 @@
 package com.github.zxh0.pbg2;
 
-import com.github.zxh0.pbg2.gen.ProtoGen;
 import com.github.zxh0.pbg2.msg.Message;
 import com.github.zxh0.pbg2.msg.field.rules.Optional;
 import com.github.zxh0.pbg2.msg.field.rules.Repeated;
@@ -9,8 +8,6 @@ import com.github.zxh0.pbg2.msg.field.types.Fixed64;
 import com.github.zxh0.pbg2.msg.field.types.Float;
 import com.github.zxh0.pbg2.msg.field.types.Int32;
 import com.github.zxh0.pbg2.msg.field.types.Sint32;
-import org.apache.commons.io.IOUtils;
-import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -27,9 +24,7 @@ public class FieldRulesTest {
 
     @Test
     public void test() throws IOException {
-        String expected = IOUtils.toString(ClassLoader.getSystemResource("pb2/field_rules.proto"));
-        String actual = ProtoGen.gen(FieldRules.class);
-        Assert.assertEquals(expected, actual);
+        TestHelper.testOutput(FieldRules.class, "pb2/field_rules_test.proto");
     }
 
 }
---
+++ code/pbg/src/test/java/com/github/zxh0/pbg2/FieldTypesTest.java
@@ -0,0 +1,39 @@
+package com.github.zxh0.pbg2;
+
+import com.github.zxh0.pbg2.msg.Message;
+import com.github.zxh0.pbg2.msg.field.rules.Required;
+import com.github.zxh0.pbg2.msg.field.types.*;
+import com.github.zxh0.pbg2.msg.field.types.Double;
+import com.github.zxh0.pbg2.msg.field.types.Float;
+import com.github.zxh0.pbg2.msg.field.types.String;
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class FieldTypesTest {
+
+    @Message
+    static class FieldTypes {
+        @Required(tag = 1) Bool f1;
+        @Required(tag = 2) Int32 f2;
+        @Required(tag = 3) Int64 f3;
+        @Required(tag = 4) Uint32 f4;
+        @Required(tag = 5) Uint64 f5;
+        @Required(tag = 6) Sint32 f6;
+        @Required(tag = 7) Sint64 f7;
+        @Required(tag = 8) Fixed32 f8;
+        @Required(tag = 9) Fixed64 f9;
+        @Required(tag = 10) Sfixed32 f10;
+        @Required(tag = 11) Sfixed64 f11;
+        @Required(tag = 12) Float f12;
+        @Required(tag = 13) Double f13;
+        @Required(tag = 14) String f14;
+        @Required(tag = 15) Bytes f15;
+    }
+
+    @Test
+    public void test() throws IOException {
+        TestHelper.testOutput(FieldTypes.class, "pb2/field_types_test.proto");
+    }
+
+}
