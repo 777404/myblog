5ee764db420fd30f67603d88d69dc8e10149cfc2
zxh0 <zhangxiuhong2013@gmail.com>
2016-01-09 11:20:57 +0800

    fix field_rules.proto

--- code/pbg/src/test/java/com/github/zxh0/pbg2/FieldRulesTest.java
+++ code/pbg/src/test/java/com/github/zxh0/pbg2/FieldRulesTest.java
@@ -9,19 +9,26 @@ import com.github.zxh0.pbg2.msg.field.types.Fixed64;
 import com.github.zxh0.pbg2.msg.field.types.Float;
 import com.github.zxh0.pbg2.msg.field.types.Int32;
 import com.github.zxh0.pbg2.msg.field.types.Sint32;
+import org.junit.Test;
 
 public class FieldRulesTest {
 
     @Message
-    static class Msg {
+    static class FieldRules {
         @Required(tag = 1) Int32 intField;
         @Repeated(tag = 2) Float f2;
         @Optional(tag = 3) Sint32 f3;
         @Optional(tag = 4) Fixed64 f4 = new Fixed64(123);
     }
 
+    @Test
+    public void test() {
+//        IOUt
+//        ClassLoader.getSystemResourceAsStream()
+    }
+
     public static void main(String[] args) {
-        String x = ProtoGen.gen(Msg.class);
+        String x = ProtoGen.gen(FieldRules.class);
         System.out.println(x);
     }
 
