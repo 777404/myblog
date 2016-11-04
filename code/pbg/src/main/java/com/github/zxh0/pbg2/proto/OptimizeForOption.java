f3b990d511bd08b1012b5ce36fd43b1a8b905284
zxh0 <zhangxiuhong2013@gmail.com>
2016-11-04 21:50:02 +0800

    -pbg

--- code/pbg/src/main/java/com/github/zxh0/pbg2/proto/OptimizeForOption.java
+++
@@ -1,8 +0,0 @@
-package com.github.zxh0.pbg2.proto;
-
-public enum OptimizeForOption {
-    NOT_GIVEN,
-    SPEED,
-    CODE_SIZE,
-    LITE_RUNTIME
-}
--- code/pbg/src/main/java/com/github/zxh0/pbg2/proto/field/types/Bytes.java
+++
@@ -1,4 +0,0 @@
-package com.github.zxh0.pbg2.proto.field.types;
-
-public class Bytes {
-}
